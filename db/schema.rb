ea04c4afd5bdab86182a0f97f23ff21c58002843
stephen2790022 <toulouse.stephen1@gmail.com>
2021-03-09 16:11:51 +0100

    add user column check: bolean

--- db/schema.rb
+++ db/schema.rb
@@ -80,6 +80,7 @@ ActiveRecord::Schema.define(version: 2021_03_08_153422) do
     t.datetime "remember_created_at"
     t.string "role"
     t.string "name"
+    t.string "checked", default: "f"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
