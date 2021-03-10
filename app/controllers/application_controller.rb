76ce93fb7520c44a4d102cad06c9b49814cd4961
dylan-pecqueux <dylan.pecqueux34@outlook.fr>
2021-03-10 12:03:04 +0100

    add index, update and routes to users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::API
     end
 
     def configure_devise_parameters
-        devise_parameter_sanitizer.permit(:sign_up) {|u| u.permit(:name, :role, :is_alive, :email, :password, :password_confirmation)}
-        devise_parameter_sanitizer.permit(:account_update) {|u| u.permit(:name, :role, :is_alive, :email, :password, :password_confirmation, :current_password)}
+        devise_parameter_sanitizer.permit(:sign_up) {|u| u.permit(:name, :role, :is_alive, :email, :checked, :password, :password_confirmation)}
+        devise_parameter_sanitizer.permit(:account_update) {|u| u.permit(:name, :role, :is_alive, :email, :checked, :password, :password_confirmation, :current_password)}
     end
 end
