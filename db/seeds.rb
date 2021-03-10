626ec6a02581b80c297b7c9233c944dbb0b1c134
Gaetan <altgaetan@hotmail.com>
2021-03-10 22:27:02 +0100

    Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+# User.destroy_all
+Formation.destroy_all
+
+10.times do
+  User.create(name: Faker::Name.name, email: Faker::Internet.email, password:"azerty", password_confirmation:"azerty", role: "student")
+end
+
+5.times do
+  User.create(name: Faker::Name.name, email: Faker::Internet.email, password:"azerty", password_confirmation:"azerty", role: "teacher")
+end
+
+1.times do
+  User.create(name: Faker::Name.name, email: Faker::Internet.email, password:"azerty", password_confirmation:"azerty", role: "admin", checked:"true")
+end
+
+# 10.times do 
+#   Formation.create(title: Faker::ProgrammingLanguage.name, description: Faker::Alphanumeric.alpha(number: 20), user_id:"user_id",)
+# end
+Formation.create(title: "hellotitle", description: "0123456789fefgfhfhhfhvjhvhjvhkvhkvhk!bgjjgcygnjvjgcghtj12345678902222", user_id: "31")
